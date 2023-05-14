c1d91862ef2831ddc94f101fa45cc5364281cba0
FiR3N <fir3norig@gmail.com>
2023-05-14 11:47:23 +0300

    AppRouter for vercel

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -5,33 +5,50 @@ import { ROUTES } from "./Routes";
 import { Suspense, lazy } from "react";
 import Loader from "../components/UI/Loader/Loader";
 import { useTypeSelector } from "../hooks/useTypeSelector";
+import Main from "../pages/Main/Main";
+import Contact from "../pages/Contact/Contact";
+import News from "../pages/News/News";
+import NewsDescription from "../pages/NewsDescription/NewsDescription";
+import Services from "../pages/Services/Services";
+import ServiceDescription from "../pages/ServiceDescription/ServiceDescription";
+import OurWorks from "../pages/OurWorks/OurWorks";
+import OurWorksDescription from "../pages/OurWorksDescription/OurWorksDescription";
+import Vacancies from "../pages/Vacancies/Vacancies";
+import Admin from "../pages/Admin/Admin";
+import AdminSettings from "../pages/AdminSettings/AdminSettings";
+import AdminLogin from "../pages/AdminLogin/AdminLogin";
+import Reviews from "../pages/Reviews/Reviews";
+import History from "../pages/History/History";
+import WorkProcess from "../pages/WorkProcess/WorkProcess";
+import About from "../pages/About/About";
+import Certificate from "../pages/Certificate/Certificate";
 
-const Contact = lazy(() => import("../pages/Contact/Contact"));
-const History = lazy(() => import("../pages/History/History"));
-const Reviews = lazy(() => import("../pages/Reviews/Reviews"));
-const WorkProcess = lazy(() => import("../pages/WorkProcess/WorkProcess"));
-const AboutCompany = lazy(() => import("../pages/AboutCompany/AboutCompany"));
-const News = lazy(() => import("../pages/News/News"));
-const NewsDescription = lazy(
-  () => import("../pages/NewsDescription/NewsDescription")
-);
-const Certificate = lazy(() => import("../pages/Certificate/Certificate"));
-const Services = lazy(() => import("../pages/Services/Services"));
-const ServiceDescription = lazy(
-  () => import("../pages/ServiceDescription/ServiceDescription")
-);
-const OurWorks = lazy(() => import("../pages/OurWorks/OurWorks"));
-const OurWorksDescription = lazy(
-  () => import("../pages/OurWorksDescription/OurWorksDescription")
-);
-const About = lazy(() => import("../pages/About/About"));
-const Vacancies = lazy(() => import("../pages/Vacancies/Vacancies"));
-const AdminLogin = lazy(() => import("../pages/AdminLogin/AdminLogin"));
-const Admin = lazy(() => import("../pages/Admin/Admin"));
-const AdminSettings = lazy(
-  () => import("../pages/AdminSettings/AdminSettings")
-);
-const Main = lazy(() => import("../pages/Main/Main"));
+// const Contact = lazy(() => import("../pages/Contact/Contact"));
+// const History = lazy(() => import("../pages/History/History"));
+// const Reviews = lazy(() => import("../pages/Reviews/Reviews"));
+// const WorkProcess = lazy(() => import("../pages/WorkProcess/WorkProcess"));
+// const AboutCompany = lazy(() => import("../pages/AboutCompany/AboutCompany"));
+// const News = lazy(() => import("../pages/News/News"));
+// const NewsDescription = lazy(
+//   () => import("../pages/NewsDescription/NewsDescription")
+// );
+// const Certificate = lazy(() => import("../pages/Certificate/Certificate"));
+// const Services = lazy(() => import("../pages/Services/Services"));
+// const ServiceDescription = lazy(
+//   () => import("../pages/ServiceDescription/ServiceDescription")
+// );
+// const OurWorks = lazy(() => import("../pages/OurWorks/OurWorks"));
+// const OurWorksDescription = lazy(
+//   () => import("../pages/OurWorksDescription/OurWorksDescription")
+// );
+// const About = lazy(() => import("../pages/About/About"));
+// const Vacancies = lazy(() => import("../pages/Vacancies/Vacancies"));
+// const AdminLogin = lazy(() => import("../pages/AdminLogin/AdminLogin"));
+// const Admin = lazy(() => import("../pages/Admin/Admin"));
+// const AdminSettings = lazy(
+//   () => import("../pages/AdminSettings/AdminSettings")
+// );
+// const Main = lazy(() => import("../pages/Main/Main"));
 
 const AppRouter = () => {
   const { isAuth } = useTypeSelector((state) => state.userReducer);
